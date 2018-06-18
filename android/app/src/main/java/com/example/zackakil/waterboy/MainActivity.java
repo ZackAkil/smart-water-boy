b9682323cab74c5e4c32bae40d33c75ad14d453b
ZackAkil <zackakil94@gmail.com>
2018-06-18 19:56:20 +0100

    impliment bits of the bluetooth code

--- android/app/src/main/java/com/example/zackakil/waterboy/MainActivity.java
+++ android/app/src/main/java/com/example/zackakil/waterboy/MainActivity.java
@@ -2,12 +2,95 @@ package com.example.zackakil.waterboy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import java.util.UUID;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.Set;
+import java.util.UUID;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
+    private BluetoothSocket soc = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InitaliseBluetooth("HC-06");
     }
+
+    @Override
+    protected void onStop() {
+        // call the superclass method first
+        super.onStop();
+        closeBluetooth();
+
+    }
+
+    private  void InitaliseBluetooth(String btDeviceName){
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Log.e("My App", "Bluetooth not found");
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                Log.d("My App", deviceName);
+                Log.d("My App", deviceHardwareAddress);
+                if(device.getName().equals(btDeviceName)) {
+                    Log.e("My App", "Found device");
+                    try{
+                        soc = createBluetoothSocket(device);
+                        soc.connect();
+                    } catch (IOException e) {
+                        Log.e("My App", "Socket creation failed");
+                    }
+
+                }
+            }
+
+        }
+    }
+
+
+    public BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        try {
+            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
+            return (BluetoothSocket) m.invoke(device, BTMODULEUUID);
+        } catch (Exception e) {
+            Log.e("My App", "Could not create Insecure RFComm Connection", e);
+        }
+        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+    }
+
+
+    public void closeBluetooth() {
+        try {
+            this.soc.close();
+
+        } catch (IOException e) {
+            Log.e("My App", "Socket creation failed", e);
+        }
+
+
+    }
+
+
 }
